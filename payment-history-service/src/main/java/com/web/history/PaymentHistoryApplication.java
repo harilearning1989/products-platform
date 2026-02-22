d4b1970dfcb7f560c8893d1af646aa2524402bc6
harilearning1989 <59171886+harilearning1989@users.noreply.github.com>
2026-02-22 13:55:45 +0530

    added all missing services

---
+++ payment-history-service/src/main/java/com/web/history/PaymentHistoryApplication.java
@@ -0,0 +1,13 @@
+package com.web.history;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentHistoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentHistoryApplication.class, args);
+	}
+
+}
---
+++ payment-history-service/src/test/java/com/web/history/PaymentHistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.history;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentHistoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
